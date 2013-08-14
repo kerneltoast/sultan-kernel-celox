1f5e49ccc1a9dee0fa2e657277a6ddfebc47020f
Dan Carpenter <dan.carpenter@oracle.com>
2013-08-14 11:50:01 +0000

    nl80211: nl80211hdr_put() doesn't return an ERR_PTR
    There are a few places which check nl80211hdr_put() for an ERR_PTR
    but actually it returns NULL on error and never error values.  In
    nl80211_testmode_dump() the return wasn't checked at all so I have
    added one.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    [some whitespace changes]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Git-commit: cb35fba360dfc3496e5d8a47e23ec5ccdfd90925
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Change-Id: I5da1aafea35110a1ce606d402b1b96859d34b869
    [akholaif@codeaurora.org: resolved conflicts manually]
    Signed-off-by: Ahmad Kholaif <akholaif@codeaurora.org>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -1808,8 +1808,8 @@ static int nl80211_get_key(struct sk_buff *skb, struct genl_info *info)
 
 	hdr = nl80211hdr_put(msg, info->snd_pid, info->snd_seq, 0,
 			     NL80211_CMD_NEW_KEY);
-	if (IS_ERR(hdr))
-		return PTR_ERR(hdr);
+	if (!hdr)
+		return -ENOBUFS;
 
 	cookie.msg = msg;
 	cookie.idx = key_idx;
@@ -4579,6 +4579,9 @@ static int nl80211_testmode_dump(struct sk_buff *skb,
 					   NL80211_CMD_TESTMODE);
 		struct nlattr *tmdata;
 
+		if (!hdr)
+			break;
+
 		if (nla_put_u32(skb, NL80211_ATTR_WIPHY, dev->wiphy_idx) < 0) {
 			genlmsg_cancel(skb, hdr);
 			break;
@@ -4973,9 +4976,8 @@ static int nl80211_remain_on_channel(struct sk_buff *skb,
 
 	hdr = nl80211hdr_put(msg, info->snd_pid, info->snd_seq, 0,
 			     NL80211_CMD_REMAIN_ON_CHANNEL);
-
-	if (IS_ERR(hdr)) {
-		err = PTR_ERR(hdr);
+	if (!hdr) {
+		err = -ENOBUFS;
 		goto free_msg;
 	}
 
@@ -5190,8 +5192,8 @@ static int nl80211_tx_mgmt(struct sk_buff *skb, struct genl_info *info)
 	hdr = nl80211hdr_put(msg, info->snd_pid, info->snd_seq, 0,
 			     NL80211_CMD_FRAME);
 
-	if (IS_ERR(hdr)) {
-		err = PTR_ERR(hdr);
+	if (!hdr) {
+		err = -ENOBUFS;
 		goto free_msg;
 	}
 	err = cfg80211_mlme_mgmt_tx(rdev, dev, chan, offchan, channel_type,
@@ -5740,9 +5742,8 @@ static int nl80211_probe_client(struct sk_buff *skb,
 
 	hdr = nl80211hdr_put(msg, info->snd_pid, info->snd_seq, 0,
 			     NL80211_CMD_PROBE_CLIENT);
-
-	if (IS_ERR(hdr)) {
-		err = PTR_ERR(hdr);
+	if (!hdr) {
+		err = -ENOBUFS;
 		goto free_msg;
 	}
 
