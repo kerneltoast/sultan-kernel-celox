34073f6a9741dac3e77e9d41b54aec961064b11f
Sultanxda <sultanxda@gmail.com>
2014-04-13 13:17:30 -0700

    msm8x60: Add 3D GPU overclocking
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -695,6 +695,16 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
+			.gpu_freq = 320000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 300000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 266667000,
 			.bus_freq = 4,
 			.io_fraction = 0,
@@ -714,13 +724,9 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
-		{
-			.gpu_freq = 27000000,
-			.bus_freq = 0,
-		},
 	},
-	.init_level = 0,
-	.num_levels = 5,
+	.init_level = 2,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12, // HZ/5 -> HZ/15, changed for low power consumption
 	.nap_allowed = true,
