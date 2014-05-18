8269bc462a9f9c0027f53a31b654dbd56aac1cac
Sultanxda <sultanxda@gmail.com>
2014-05-18 00:16:50 -0700

    msm_fb: Move device-specific display drivers to mach-msm
    Also disabled mdp4_video_enhance as it does nothing. Moving the device-specific drivers to mach-msm makes it easier to work with the framebuffer drivers.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- arch/arm/mach-msm/display/lcdc_ld9040.c
+++ arch/arm/mach-msm/display/lcdc_ld9040.c
@@ -62,7 +62,6 @@
 #include <linux/fb.h>
 #include <linux/backlight.h>
 #include <linux/miscdevice.h>
-#include "mdp4_video_enhance.h"
 #include <linux/regulator/consumer.h>
 
 #define MAPPING_TBL_AUTO_BRIGHTNESS 1
@@ -2251,8 +2250,6 @@ static int __devinit ld9040_probe(struct platform_device *pdev)
 		DPRINT("lcd_power failed to add sysfs entries\n");
 //		dev_err(&(pdev->dev), "failed to add sysfs entries\n");
 
-	// mdnie sysfs create
-	init_mdnie_class();
 ////////////
 
 #ifdef MAPPING_TBL_AUTO_BRIGHTNESS
