c2224c8da73595cdb6daf0660999bf6e72b45554
Sultanxda <sultanxda@gmail.com>
2014-06-11 17:25:52 -0700

    gpu: ion: Conditionally issue kernel WARNING in ion_unmap_iommu()
    We're expecting this sometimes when unmapping for domain 2 & partition 2, so no reason to print a big warning for it.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -746,8 +746,13 @@ int ion_unmap_iommu(struct ion_client *client, struct ion_handle *handle,
 	iommu_map = ion_iommu_lookup(buffer, domain_num, partition_num);
 
 	if (!iommu_map) {
-		WARN(1, "%s: (%d,%d) was never mapped for %p\n", __func__,
-				domain_num, partition_num, buffer);
+		if ((domain_num == 2) && (partition_num == 2))
+			pr_err("%s: (%d,%d) was never mapped for %p\n", __func__,
+					domain_num, partition_num, buffer);
+		else
+			WARN(1, "%s: (%d,%d) was never mapped for %p\n", __func__,
+					domain_num, partition_num, buffer);
+
 		mutex_unlock(&buffer->lock);
 		mutex_unlock(&client->lock);
 		return -EINVAL;
