f7ba15786e3c63697fea5a2080755cf3f0827888
Sultanxda <sultanxda@gmail.com>
2014-07-11 02:49:13 -0700

    msm: mdp4: Enable release fence for LCDC panels
    Makes graphics smoother.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -4012,7 +4012,7 @@ int mdp4_overlay_commit(struct fb_info *info)
 		break;
 	case LVDS_PANEL:
 	case LCDC_PANEL:
-		mdp4_lcdc_pipe_commit(0, 1);
+		mdp4_lcdc_pipe_commit(0, 1, &release_busy);
 		break;
 	case DTV_PANEL:
 		mdp4_dtv_pipe_commit(0, 1);
