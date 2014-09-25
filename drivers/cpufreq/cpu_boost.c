7b9408b3934d3602d30dc04296aa5f54ad8cb04e
Sultanxda <sultanxda@gmail.com>
2014-09-25 15:04:04 -0700

    cpufreq: Introduce CPU-boost framework
    The CPU-boost framework allows you to place hooks in various drivers to boost the CPU for either a specified duration or indefinitely (until it is manually unboosted). If a new boost request is sent while the CPU is boosted then the new boost request will take precedence and immediately override the current boost.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

---
+++ drivers/cpufreq/cpu_boost.c
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2014, Sultanxda <sultanxda@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/cpu.h>
+#include <linux/cpu_boost.h>
+#include <linux/cpufreq.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
+static struct delayed_work boost_work;
+
+static DECLARE_COMPLETION(cpu_boost_no_timeout);
+
+static unsigned int boost_duration_ms = 0;
+static unsigned int boost_freq_khz = 0;
+static unsigned int boost_override = 0;
+static unsigned int cpu_boosted = 0;
+static unsigned int init_done = 0;
+static unsigned int minfreq_orig = 0;
+
+void cpu_boost_timeout(unsigned int freq_mhz, unsigned int duration_ms)
+{
+	if (init_done) {
+		if (cpu_boosted) {
+			cpu_boosted = 0;
+			boost_override = 1;
+			cancel_delayed_work(&boost_work);
+		}
+
+		boost_freq_khz = freq_mhz * 1000;
+		boost_duration_ms = duration_ms;
+		schedule_delayed_work(&boost_work, 0);
+	}
+}
+
+void cpu_boost(unsigned int freq_mhz)
+{
+	if (init_done) {
+		if (cpu_boosted) {
+			cpu_boosted = 0;
+			boost_override = 1;
+			cancel_delayed_work(&boost_work);
+		}
+
+		init_completion(&cpu_boost_no_timeout);
+		boost_freq_khz = freq_mhz * 1000;
+		schedule_delayed_work(&boost_work, 0);
+	}
+}
+
+void cpu_unboost(void)
+{
+	if (init_done)
+		complete(&cpu_boost_no_timeout);
+}
+
+static void save_original_minfreq(void)
+{
+	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
+
+	minfreq_orig = policy->user_policy.min;
+}
+
+static void restore_original_minfreq(void)
+{
+	struct cpufreq_policy *policy = NULL;
+	unsigned int cpu = 0;
+
+	get_online_cpus();
+	for_each_online_cpu(cpu) {
+		policy = cpufreq_cpu_get(cpu);
+		cpufreq_verify_within_limits(policy, minfreq_orig, UINT_MAX);
+		policy->user_policy.min = minfreq_orig;
+		cpufreq_update_policy(cpu);
+		cpufreq_cpu_put(policy);
+	}
+	put_online_cpus();
+
+	boost_duration_ms = 0;
+	cpu_boosted = 0;
+	boost_override = 0;
+}
+
+static void __cpuinit cpu_boost_main(struct work_struct *work)
+{
+	struct cpufreq_policy *policy = NULL;
+	unsigned int cpu = 0, minfreq = 0, wait_ms = 0;
+
+	if (cpu_boosted) {
+		restore_original_minfreq();
+		return;
+	}
+
+	if (!boost_override)
+		save_original_minfreq();
+
+	if (boost_freq_khz) {
+		minfreq = boost_freq_khz;
+
+		/* boost online CPUs */
+		get_online_cpus();
+		for_each_online_cpu(cpu) {
+			policy = cpufreq_cpu_get(cpu);
+			cpufreq_verify_within_limits(policy, minfreq, UINT_MAX);
+			policy->user_policy.min = minfreq;
+			cpufreq_update_policy(cpu);
+			cpufreq_cpu_put(policy);
+		}
+		put_online_cpus();
+		cpu_boosted = 1;
+	}
+
+	if (boost_duration_ms)
+		wait_ms = boost_duration_ms;
+	else
+		wait_for_completion(&cpu_boost_no_timeout);
+
+
+	schedule_delayed_work(&boost_work,
+				msecs_to_jiffies(wait_ms));
+}
+
+static int __init cpu_boost_init(void)
+{
+	INIT_DELAYED_WORK(&boost_work, cpu_boost_main);
+
+	init_done = 1;
+
+	return 0;
+}
+late_initcall(cpu_boost_init);
+
+MODULE_AUTHOR("Sultanxda <sultanxda@gmail.com>");
+MODULE_DESCRIPTION("CPU-boost framework");
+MODULE_LICENSE("GPLv2");
---
+++ include/linux/cpu_boost.h
@@ -0,0 +1,46 @@
+/*
+ * include/linux/cpu_boost.h
+ *
+ * Copyright (c) 2014, Sultanxda <sultanxda@gmail.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_CPU_BOOST_H
+#define _LINUX_CPU_BOOST_H
+
+
+#ifdef CONFIG_CPU_BOOST_FRAMEWORK
+/**
+ * cpu_boost_timeout() - boost online CPUs for a given amount of time
+ * @freq_mhz:	frequency in MHz to boost the CPUs by
+ * @duration_ms: duration in milliseconds to boost the CPUs
+ */
+void cpu_boost_timeout(unsigned int freq_mhz, unsigned int duration_ms);
+
+/**
+ * cpu_boost() - boost online CPUs indefinitely
+ * @freq_mhz:	frequency in MHz to boost the CPUs by
+ */
+void cpu_boost(unsigned int freq_mhz);
+
+/**
+ * cpu_unboost() - unboost indefinitely-boosted CPUs
+ */
+void cpu_unboost(void);
+
+#else
+static inline void cpu_boost_timeout(unsigned int freq_mhz, unsigned int duration_ms) { }
+static inline void cpu_boost(unsigned int freq_mhz) { }
+static inline void cpu_unboost(void) { }
+
+#endif /* CONFIG_CPU_BOOST_FRAMEWORK */
+#endif /* _LINUX_CPU_BOOST_H */
