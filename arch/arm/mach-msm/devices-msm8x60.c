0b041adfb5c9a9f2380c436815269a603ac527e8
Sultanxda <sultanxda@gmail.com>
2014-09-29 18:35:55 -0700

    devices-msm8x60: Add back 27MHz GPU slumber frequency

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -794,6 +794,10 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
+		{
+			.gpu_freq = 27000000,
+			.bus_freq = 0,
+		},
 	},
 	.init_level = 2,
 	.num_levels = 7,
