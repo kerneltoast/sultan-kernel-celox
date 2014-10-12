c36c9d9ef55d37b3b5a11758ff023cdb374a67d3
Sultanxda <sultanxda@gmail.com>
2014-10-12 12:18:37 -0700

    cpu_boost: Fix unbalanced cpufreq_cpu_get
    Also, minor clean-up.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/cpufreq/cpu_boost.c
+++ drivers/cpufreq/cpu_boost.c
@@ -89,6 +89,8 @@ static void save_original_cpu_limits(void)
 
 	minfreq_orig = policy->user_policy.min;
 	maxfreq_orig = policy->user_policy.max;
+
+	cpufreq_cpu_put(policy);
 }
 
 static void set_new_minfreq(struct cpufreq_policy *policy,
@@ -102,14 +104,12 @@ static void restore_original_minfreq(void)
 	struct cpufreq_policy *policy = NULL;
 	unsigned int cpu = 0;
 
-	get_online_cpus();
 	for_each_online_cpu(cpu) {
 		policy = cpufreq_cpu_get(cpu);
 		set_new_minfreq(policy, minfreq_orig);
 		cpufreq_update_policy(cpu);
 		cpufreq_cpu_put(policy);
 	}
-	put_online_cpus();
 
 	boost_duration_ms = 0;
 	cpu_boosted = 0;
@@ -130,7 +130,6 @@ static void __cpuinit cpu_boost_main(struct work_struct *work)
 		save_original_cpu_limits();
 
 	if (boost_freq_khz) {
-
 		if (boost_freq_khz >= maxfreq_orig) {
 			if (maxfreq_orig <= 486000) {
 				boost_duration_ms = 0;
@@ -142,14 +141,12 @@ static void __cpuinit cpu_boost_main(struct work_struct *work)
 			minfreq = boost_freq_khz;
 
 		/* boost online CPUs */
-		get_online_cpus();
 		for_each_online_cpu(cpu) {
 			policy = cpufreq_cpu_get(cpu);
 			set_new_minfreq(policy, minfreq);
 			cpufreq_update_policy(cpu);
 			cpufreq_cpu_put(policy);
 		}
-		put_online_cpus();
 		cpu_boosted = 1;
 	}
 
