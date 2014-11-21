f1df3ff6346fb449baa0bf35b2ec5ad420f9151d
Sultanxda <sultanxda@gmail.com>
2014-11-20 19:17:14 -0800

    cpu_input_boost: Add keypad boosting
    Thanks to CAF for this mode.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -195,6 +195,11 @@ static const struct input_device_id cpu_boost_ids[] = {
 		.absbit = { [BIT_WORD(ABS_X)] =
 			BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
 	},
+	/* keypad */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+		.evbit = { BIT_MASK(EV_KEY) },
+	},
 	{ },
 };
 
