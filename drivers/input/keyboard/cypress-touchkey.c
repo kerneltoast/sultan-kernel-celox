e8dff3baa36c3351bb811a87f5277977e83f5442
Sultanxda <sultanxda@gmail.com>
2015-01-26 20:40:03 -0800

    Revert "input: cypress_touchkey: Fix NULL pointer in i2c_touchkey_write()"
    This reverts commit 997f72d17ac6beb8a21f575aa672621dfecc4ddd.
    
    Derp, I see what this is used for now. This is necessary.

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -205,6 +205,7 @@ struct i2c_touchkey_driver {
 	struct input_dev *input_dev;
 	struct early_suspend early_suspend;
 	struct mutex mutex;
+	bool is_bln_active;
 };
 struct i2c_touchkey_driver *touchkey_driver = NULL;
 struct work_struct touchkey_work;
@@ -302,7 +303,11 @@ static int i2c_touchkey_write(u8 * val, unsigned int len)
 	struct i2c_msg msg[1];
 	int retry = 2;
 
-	if ((touchkey_driver == NULL || touchkey_enable != 1)) {
+	if ((touchkey_driver == NULL || touchkey_enable != 1)
+#if defined(CONFIG_ENHANCED_BLN)
+		&& (!touchkey_driver->is_bln_active)
+#endif
+	) {
 		printk(KERN_DEBUG "[TKEY] touchkey is not enabled.W\n");
 		return -ENODEV;
 	}
@@ -1065,6 +1070,7 @@ static void cypress_touchkey_enable_led_vdd(void)
 	tkey_vdd_enable(1);
 	msleep(50);
 	tkey_led_vdd_enable(1);
+	touchkey_driver->is_bln_active = true;
 	mutex_unlock(&touchkey_driver->mutex);
 }
 
@@ -1073,6 +1079,7 @@ static void cypress_touchkey_disable_led_vdd(void)
 	mutex_lock(&touchkey_driver->mutex);
 	tkey_vdd_enable(0);
 	tkey_led_vdd_enable(0);
+	touchkey_driver->is_bln_active = false;
 	mutex_unlock(&touchkey_driver->mutex);
 }
 
