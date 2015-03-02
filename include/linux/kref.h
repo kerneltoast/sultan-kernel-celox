ca3231c4b8706204e4f9676f38ec088016ba3947
Sultanxda <sultanxda@gmail.com>
2015-03-01 21:15:34 -0800

    msm: kgsl: Import from CAF LA.AF.1.1_rb1.12
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- include/linux/kref.h
+++ include/linux/kref.h
@@ -15,6 +15,7 @@
 #ifndef _KREF_H_
 #define _KREF_H_
 
+#include <linux/atomic.h>
 #include <linux/types.h>
 
 struct kref {
@@ -27,4 +28,8 @@ int kref_put(struct kref *kref, void (*release) (struct kref *kref));
 int kref_sub(struct kref *kref, unsigned int count,
 	     void (*release) (struct kref *kref));
 
+static inline int __must_check kref_get_unless_zero(struct kref *kref)
+{
+	return atomic_add_unless(&kref->refcount, 1, 0);
+}
 #endif /* _KREF_H_ */
