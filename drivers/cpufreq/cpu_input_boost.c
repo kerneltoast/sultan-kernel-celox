5965b079ca227e1bcf3d35d8262e7ea6464154c3
Sultanxda <sultanxda@gmail.com>
2015-05-09 18:36:19 -0700

    cpu_input_boost: Specify some variables as read-mostly

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -19,7 +19,6 @@
 #include <linux/hrtimer.h>
 #include <linux/input.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/notifier.h>
 #include <linux/slab.h>
 
@@ -42,10 +41,10 @@ static DEFINE_MUTEX(boost_mutex);
 
 static bool boost_running;
 static bool suspended;
-static bool freqs_available;
-static unsigned int boost_freq[3];
+static bool freqs_available __read_mostly;
+static unsigned int boost_freq[3] __read_mostly;
 static unsigned int boost_ms[3];
-static unsigned int enabled;
+static unsigned int enabled __read_mostly;
 
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
